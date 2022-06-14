818ce9f4a2dd20557fd0ae2b028c80122a225713
Angeltromper <atromper@outlook.com>
2022-06-14 13:03:59 +0200

    huiswerk-les-6

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import logo from '../assets/banana-01.png';
 import { useHistory, Link } from 'react-router-dom';
+import {AuthContext} from "../context/AuthContext";
 
 function NavBar() {
-  const history = useHistory();
+  const history = useHistory ();
+
+  const {auth} = useContext ( AuthContext );
 
   return (
-    <nav>
+      <nav>
         <Link to="/">
           <span className="logo-container">
             <img src={logo} alt="logo"/>
@@ -16,22 +19,35 @@ function NavBar() {
           </span>
         </Link>
 
-      <div>
-        <button
-          type="button"
-          onClick={() => history.push('/signin')}
-        >
-          Log in
-        </button>
-        <button
-          type="button"
-          onClick={() => history.push('/signup')}
-        >
-          Registreren
-        </button>
-      </div>
-    </nav>
+        <div>
+
+          {auth === false ?
+
+              <button type="button"
+                      onClick={() => history.push ( '/signin' )}
+              >
+                Login
+              </button>:
+
+              <button
+                  type="button"
+                  onClick={() => history.push ( '/signin' )}
+              >
+                Uitloggen
+              </button>
+          }
+
+          <button
+              type="button"
+              onClick={() =>
+                  history.push ( '/signup' )}
+          >
+            Registreren
+          </button>
+        </div>
+      </nav>
   );
 }
+
 
 export default NavBar;
---
+++ src/context/AuthContext.js
@@ -0,0 +1,47 @@
+import React, {createContext, useState} from "react";
+import { useHistory } from 'react-router-dom';
+
+export const AuthContext = createContext(null);
+
+
+function AuthContextProvider({children}) {
+    const [isAuth, toggleIsAuth] =
+ useState(false);
+    const history = useHistory();
+
+
+    function login() {
+        console.log ( "Gebruiker is ingelogd" );
+        toggleIsAuth ( !isAuth );
+        //history.push('/profile');
+    }
+
+    function logout() {
+        console.log ( "gebruiker is uitgelogd" );
+        toggleIsAuth ( !isAuth );
+        //history.push('/')
+    }
+
+    return (
+        <AuthContext.Provider value={
+
+            {
+                auth: isAuth,
+                login: login,
+                logout: logout,
+            }
+        }>
+            {children}
+        </AuthContext.Provider>
+
+    );
+}
+
+export default AuthContextProvider;
+
+
+
+
+
+
+
